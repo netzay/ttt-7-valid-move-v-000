61a24d6d67bab0169fd0d218aadb42fdc7321344
Ally Netzel <netzelally@gmail.com>
2018-03-22 23:37:49 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,13 +3,6 @@ def valid_move?(board, index)
   if position_taken?(board, index) == false 
     return true
   end
-  if board[index].between?(0,8) != true
-    return false
-  end
-  if position_taken?(board, index) or board[index].between?(0,8)== true
-    return false
-  end
-  
 end
 
 
